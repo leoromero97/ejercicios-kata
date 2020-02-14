403374c97d2a19f0d17bad96f9cebb05cb0a7f30
leonardo97 <leonardogerbacio@gmail.com>
2020-02-14 13:58:53 -0300

    realice ejercicio con jowie

---
+++ ejer.js
@@ -0,0 +1,11 @@
+function mayus(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+'hola tengo hambre'.replace(/\b\w/g, function(1){
+    return 1.toUpperCase() 
+}
+    );
+
+    'hola tengo hambre'.replace(/\b\w/g, 1 => 1.toUpperCase())
