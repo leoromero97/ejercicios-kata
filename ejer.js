891ac81867cb33404911afffe7a0f3f8b3c7a0e0
vivianachoque <vivianaachoque@gmail.com>
2020-03-02 16:42:41 -0300

    realice ejercicio

--- ejer.js
+++ ejer.js
@@ -1,14 +1,7 @@
-let array = [1,2,3,4];
-let indicador = 2;
-
-function shiftArray(array,indicador) {
-    newArray = "";
-    recorrer = "";
-    for (let i = 0; i<array.length ; i++) {
-        recorrer = recorrer + array[i] + ' , '; 
-    }
-    console.log(recorrer);
-    newArray = array.shift(indicador);
-    console.log(newArray);
-    console.log(array + ',' + newArray);
-}
+function arrayShift(arr,n) {
+    for(let i=0; i<n;i++){
+        arr.push(arr.shift());
+      }
+      return arr;
+  }
+  console.log( arrayShift([1,2,3,4,5,6],2));
